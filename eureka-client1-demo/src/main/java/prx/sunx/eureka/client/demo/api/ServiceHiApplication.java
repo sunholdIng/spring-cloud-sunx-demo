d052d62ff59b9d7c593299fe21b2ca54abdccaac
sunx <sunx@drore.com>
2017-11-22 14:55:55 +0800

    添加测试客户端

---
+++ eureka-client1-demo/src/main/java/prx/sunx/eureka/client/demo/api/ServiceHiApplication.java
@@ -0,0 +1,24 @@
+package prx.sunx.eureka.client.demo.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 浙江卓锐科技有限公司
+ * <p>
+ * Created by sunx on 2017/11/21.
+ * Description:
+ */
+@RestController
+public class ServiceHiApplication {
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hello")
+    public String home(@RequestParam String name) {
+        return "hello "+name+",i am from port:" +port;
+    }
+}
