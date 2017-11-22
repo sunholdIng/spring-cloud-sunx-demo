08d85f9d8b6fe65700e3d31185014ff37dd3758d
sunx <sunx@drore.com>
2017-11-22 16:33:48 +0800

    feign方式调用

--- service-ribbon-demo/src/main/java/prx/sunx/ribbon/demo/ServiceRibbonDemoApplication.java
+++ service-ribbon-demo/src/main/java/prx/sunx/ribbon/demo/ServiceRibbonDemoApplication.java
@@ -3,9 +3,12 @@ package prx.sunx.ribbon.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+//断路器注解配置
+@EnableHystrix
 public class ServiceRibbonDemoApplication {
 
 	public static void main(String[] args) {
--- service-ribbon-demo/src/main/java/prx/sunx/ribbon/demo/service/HelloService.java
+++ service-ribbon-demo/src/main/java/prx/sunx/ribbon/demo/service/HelloService.java
@@ -1,5 +1,6 @@
 package prx.sunx.ribbon.demo.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +14,13 @@ public class HelloService {
     @Autowired
     private RestTemplate restTemplate;
 
+    //注解表示断路器（如服务器连接不上时候）配置，失败调用固定的方法，如：hiError
+    @HystrixCommand(fallbackMethod = "helloError")
     public String hiService(String name){
         return restTemplate.getForObject("http://service-client1/hello?name="+name,String.class);
+    }
+
+    public String helloError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
